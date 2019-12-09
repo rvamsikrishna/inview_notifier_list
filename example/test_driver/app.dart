68d917e7ec835c9bc930697d0e57281a53449ba5
panxz <iptton@gmail.com>
2019-12-09 16:02:58 +0800

    add/remove files

---
+++ example/test_driver/app.dart
@@ -0,0 +1,10 @@
+
+import 'package:example/main.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_driver/driver_extension.dart';
+
+void main(){
+  enableFlutterDriverExtension();
+
+  runApp(MyApp());
+}
--- test_driver/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_driver/driver_extension.dart';
-
-import '../example/lib/main.dart' as example;
-
-void main() {
-  enableFlutterDriverExtension();
-
-  example.main();
-}
