fe3427fe70f71945f5a8c8a6f07358ba97e37996
rvamsikrishna <rvamsi1993@gmail.com>
2020-04-04 14:08:18 +0530

    updated ci

--- scripts/flutter_test.sh
+++
@@ -1,7 +0,0 @@
-cd $1
-flutter packages get
-flutter format --set-exit-if-changed --line-length 120 lib test
-flutter analyze --no-current-package lib test/
-flutter test --no-pub --coverage
-# resets to the original state
-cd -
