2a2752b38293aa5ca3d184001a8445722c98ea89
Vamsi Krishna <vamsi@kleargroup.com>
2021-12-28 17:35:41 +0530

    updated gitignore

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_ROOT=C:\Users\rvams\fvm\versions\2.5.3"
 export "FLUTTER_APPLICATION_PATH=E:\vamsi\workspace\inview_notifier_list\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
